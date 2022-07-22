be5fd3c4e03c39c84b4b0cd578cbb40229cc3d47
Tim <Tim280696@hotmail.com>
2022-07-22 15:37:10 +0200

    user: create user model

---
+++ src/models/user.ts
@@ -0,0 +1,85 @@
+// @ts-ignore
+import Client from "../database";
+
+export type User = {
+  id: number;
+  user_name: string;
+  first_name: string;
+  last_name: string;
+  password: string;
+};
+
+export class UserStore {
+  async index(): Promise<User[]> {
+    try {
+      // @ts-ignore
+      const conn = await Client.connect();
+      const sql = "SELECT * FROM users";
+
+      const result = await conn.query(sql);
+
+      conn.release();
+
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Could not get users. Error: ${err}`);
+    }
+  }
+
+  async show(id: string): Promise<User> {
+    try {
+      const sql = "SELECT * FROM users WHERE id=($1)";
+      // @ts-ignore
+      const conn = await Client.connect();
+
+      const result = await conn.query(sql, [id]);
+
+      conn.release();
+
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Could not find user ${id}. Error: ${err}`);
+    }
+  }
+
+  async create(b: User): Promise<User> {
+    try {
+      const sql =
+        "INSERT INTO users (first_name, last_name, password) VALUES($1, $2, $3) RETURNING *";
+      // @ts-ignore
+      const conn = await Client.connect();
+
+      const result = await conn.query(sql, [
+        b.first_name,
+        b.last_name,
+        b.password,
+      ]);
+
+      const user = result.rows[0];
+
+      conn.release();
+
+      return user;
+    } catch (err) {
+      throw new Error(`Could not add new user ${b.first_name}. Error: ${err}`);
+    }
+  }
+
+  async delete(id: string): Promise<User> {
+    try {
+      const sql = "DELETE FROM users WHERE id=($1)";
+      // @ts-ignore
+      const conn = await Client.connect();
+
+      const result = await conn.query(sql, [id]);
+
+      const user = result.rows[0];
+
+      conn.release();
+
+      return user;
+    } catch (err) {
+      throw new Error(`Could not delete user ${id}. Error: ${err}`);
+    }
+  }
+}
