dc8644a18ab5b99cf4ad76ac8c1873d05fbac00f
Tim <Tim280696@hotmail.com>
2022-07-27 12:24:43 +0200

    server: import userRoutes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import api from "./routes/index";
+import userRoutes from "./handlers/user";
 
 const app: express.Application = express();
 const address: string = "0.0.0.0:3000";
@@ -12,6 +13,8 @@ app.get("/", function (req: Request, res: Response) {
 });
 
 app.use("/api", api);
+
+userRoutes(app);
 
 app.listen(3000, function () {
   console.log(`starting app on: ${address}`);
