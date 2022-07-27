97d64b4550f76245ea504509456c8e023338cce5
Tim <Tim280696@hotmail.com>
2022-07-27 12:26:08 +0200

    index: remove user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,9 @@
 import express from "express";
-import { users } from "./api/users";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("This is the api route.");
 });
-
-router.use("/users", users);
 
 export default router;
