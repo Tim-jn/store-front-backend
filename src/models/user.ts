3adcea19e99774f246c1320daabd881ba114a3e8
Tim <Tim280696@hotmail.com>
2022-07-29 12:23:52 +0200

    user: Client => client

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import Client from "../database";
+import client from "../database";
 
 export type User = {
   id: number;
@@ -12,7 +12,7 @@ export class UserStore {
   async index(): Promise<User[]> {
     try {
       // @ts-ignore
-      const conn = await Client.connect();
+      const conn = await client.connect();
       const sql = "SELECT * FROM users";
 
       const result = await conn.query(sql);
@@ -24,12 +24,11 @@ export class UserStore {
       throw new Error(`Could not get users. Error: ${err}`);
     }
   }
-
   async show(id: string): Promise<User> {
     try {
       const sql = "SELECT * FROM users WHERE id=($1)";
       // @ts-ignore
-      const conn = await Client.connect();
+      const conn = await client.connect();
 
       const result = await conn.query(sql, [id]);
 
@@ -46,7 +45,7 @@ export class UserStore {
       const sql =
         "INSERT INTO users (first_name, last_name, password) VALUES($1, $2, $3) RETURNING *";
       // @ts-ignore
-      const conn = await Client.connect();
+      const conn = await client.connect();
 
       const result = await conn.query(sql, [
         b.first_name,
@@ -68,7 +67,7 @@ export class UserStore {
     try {
       const sql = "DELETE FROM users WHERE id=($1)";
       // @ts-ignore
-      const conn = await Client.connect();
+      const conn = await client.connect();
 
       const result = await conn.query(sql, [id]);
 
