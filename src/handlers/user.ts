e1d6d3a7a3dc5337ee30c1694aec663023930a9d
Tim <Tim280696@hotmail.com>
2022-07-29 12:23:33 +0200

    user: add userRoutes

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -10,8 +10,38 @@ const index = async (_req: Request, res: Response) => {
   res.json(user);
 };
 
+const show = async (req: Request, res: Response) => {
+  const user = await store.show(req.params.id);
+  res.json(user);
+};
+
+const create = async (req: Request, res: Response) => {
+  try {
+    const user: User = {
+      id: req.body.id,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      password: req.body.password,
+    };
+
+    const newUser = await store.create(user);
+    res.json(newUser);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const destroy = async (req: Request, res: Response) => {
+  const deleted = await store.delete(req.body.id);
+  res.json(deleted);
+};
+
 const userRoutes = (app: express.Application) => {
   app.get("/users", index);
+  app.get("/users/:id", show);
+  app.post("/users", create);
+  app.delete("/users/:id", destroy);
 };
 
 export default userRoutes;
