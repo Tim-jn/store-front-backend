22c92fa95cee6c50b8e6c8a50dba2dbf356b2f03
Tim <Tim280696@hotmail.com>
2022-08-01 16:00:18 +0200

    server: add productsroutes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import userRoutes from "./handlers/users";
+import usersRoutes from "./handlers/users";
+import productsRoutes from "./handlers/products";
 
 const app: express.Application = express();
 const address: string = "0.0.0.0:3000";
@@ -11,7 +12,8 @@ app.get("/", function (req: Request, res: Response) {
   res.send("Hello World!");
 });
 
-userRoutes(app);
+usersRoutes(app);
+productsRoutes(app);
 
 app.listen(3000, function () {
   console.log(`starting app on: ${address}`);
