cd903508b831238c35df20cc91dce4613a1f8698
Tim <Tim280696@hotmail.com>
2022-08-01 16:00:37 +0200

    create products handler

---
+++ src/handlers/products.ts
@@ -0,0 +1,67 @@
+import express, { Request, Response } from "express";
+import { Product, ProductStore } from "../models/products";
+import verifyAuthToken from "../middlewares/verifyAuthToken";
+import jwt from "jsonwebtoken";
+
+export const store = new ProductStore();
+
+// Index route
+
+const index = async (_req: Request, res: Response) => {
+  const product = await store.index();
+  res.json(product);
+};
+
+const show = async (req: Request, res: Response) => {
+  const product = await store.show(req.params.id);
+  res.json(product);
+};
+
+const create = async (req: Request, res: Response) => {
+  try {
+    const product: Product = {
+      id: req.body.id,
+      name: req.body.name,
+      price: req.body.price,
+    };
+
+    const newProduct = await store.create(product);
+    var token = jwt.sign(
+      { product: newProduct },
+      process.env.TOKEN_SECRET as string
+    );
+    res.json(token);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const destroy = async (req: Request, res: Response) => {
+  try {
+    const authorizationHeader = req.headers.authorization as string;
+    const token = authorizationHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN_SECRET as string);
+  } catch (err) {
+    res.status(401);
+    res.json("Access denied, invalid token");
+    return;
+  }
+
+  try {
+    const deleted = await store.delete(req.params.id);
+    res.json(deleted);
+  } catch (error) {
+    res.status(400);
+    res.json({ error });
+  }
+};
+
+const productsRoute = (app: express.Application) => {
+  app.get("/products", index);
+  app.get("/products/:id", show);
+  app.post("/products", create);
+  app.delete("/products/:id", verifyAuthToken, destroy);
+};
+
+export default productsRoute;
