8132d89a132ede73c018169c006dd9b459ef9bc6
Tim <Tim280696@hotmail.com>
2022-08-02 12:06:53 +0200

    orders: create model

---
+++ src/models/orders.ts
@@ -0,0 +1,24 @@
+// @ts-ignore
+import client from "../database";
+
+export type Order = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+export class OrdersStore {
+  async show(id: number): Promise<Order[]> {
+    try {
+      // @ts-ignore
+      const conn = await client.connect();
+      const sql = `SELECT * FROM orders WHERE user_id=($1)`;
+      const result = await conn.query(sql, [id]);
+      conn.release();
+
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Could not get orders. Error: ${err}`);
+    }
+  }
+}
