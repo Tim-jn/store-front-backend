a798ef9ac40fcff770c8b23c78c45e513d33a49c
Tim <Tim280696@hotmail.com>
2022-08-02 21:12:00 +0200

    update table

--- migrations/sqls/20220722090118-users-up.sql
+++ migrations/sqls/20220722090118-users-up.sql
@@ -1,1 +1,1 @@
-CREATE TABLE users(id SERIAL PRIMARY KEY, user_name VARCHAR(100), first_name VARCHAR(100), last_name VARCHAR(100), password VARCHAR(100));
+CREATE TABLE users(id SERIAL PRIMARY KEY, first_name VARCHAR(100), last_name VARCHAR(100), password VARCHAR(100));
