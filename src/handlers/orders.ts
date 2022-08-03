b8253a4b55b68aabdfcc4af195c3e38794a93ea4
Tim <Tim280696@hotmail.com>
2022-08-03 15:32:53 +0200

    orders: create addOrders func

--- src/handlers/orders.ts
+++ src/handlers/orders.ts
@@ -6,8 +6,26 @@ export const store = new OrdersStore();
 
 const show = async (req: Request, res: Response) => {
   try {
-    const orders = await store.show(req.body.id as number);
+    const orders = await store.show(req.body.user_id as number);
     res.json(orders);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const addOrders = async (req: Request, res: Response) => {
+  try {
+    const order: Order = {
+      user_id: req.body.user_id,
+      product_id: req.body.product_id,
+      quantity: req.body.quantity,
+      status: req.body.status,
+    };
+
+    const newOrder = await store.addOrder(order);
+
+    res.json(newOrder);
   } catch (err) {
     res.status(400);
     res.json(err);
@@ -16,6 +34,7 @@ const show = async (req: Request, res: Response) => {
 
 const orderRoutes = (app: express.Application) => {
   app.get("/orders/:id", verifyAuthToken, show);
+  app.post("/orders/:id/products", verifyAuthToken, addOrders);
 };
 
 export default orderRoutes;
