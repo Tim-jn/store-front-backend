e6d2fecd47fd439ec0f6f6ae757ac3043bac6fcd
Tim <Tim280696@hotmail.com>
2022-08-04 13:27:24 +0200

    orders: add createOrder

--- src/handlers/orders.ts
+++ src/handlers/orders.ts
@@ -1,13 +1,27 @@
 import express, { Request, Response } from "express";
-import { Order, OrdersStore } from "../models/orders";
+import { Order, OrderProducts, OrdersStore } from "../models/orders";
 import verifyAuthToken from "../middlewares/verifyAuthToken";
 
 export const store = new OrdersStore();
 
 const show = async (req: Request, res: Response) => {
   try {
-    const orders = await store.show(req.body.user_id as number);
+    const orders = await store.show(req.body.id);
     res.json(orders);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const createOrder = async (req: Request, res: Response) => {
+  try {
+    const newOrder = await store.create({
+      user_id: req.body.user_id,
+      status: req.body.status,
+    });
+
+    res.json(newOrder);
   } catch (err) {
     res.status(400);
     res.json(err);
@@ -16,14 +30,11 @@ const show = async (req: Request, res: Response) => {
 
 const addOrders = async (req: Request, res: Response) => {
   try {
-    const order: Order = {
-      user_id: req.body.user_id,
+    const newOrder = await store.addOrder({
+      order_id: req.body.order_id,
       product_id: req.body.product_id,
       quantity: req.body.quantity,
-      status: req.body.status,
-    };
-
-    const newOrder = await store.addOrder(order);
+    });
 
     res.json(newOrder);
   } catch (err) {
@@ -34,6 +45,7 @@ const addOrders = async (req: Request, res: Response) => {
 
 const orderRoutes = (app: express.Application) => {
   app.get("/orders/:id", verifyAuthToken, show);
+  app.post("/orders", verifyAuthToken, createOrder);
   app.post("/orders/:id/products", verifyAuthToken, addOrders);
 };
 
