4c3f99a79fef47a39db5d58782d64dd916c88a98
Tim <Tim280696@hotmail.com>
2022-08-04 18:48:40 +0200

    products: add try/catch

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -8,13 +8,23 @@ export const store = new ProductStore();
 // Index route
 
 const index = async (_req: Request, res: Response) => {
-  const product = await store.index();
-  res.json(product);
+  try {
+    const product = await store.index();
+    res.json(product);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
 };
 
 const show = async (req: Request, res: Response) => {
-  const product = await store.show(req.params.id);
-  res.json(product);
+  try {
+    const product = await store.show(req.params.id);
+    res.json(product);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
 };
 
 const create = async (req: Request, res: Response) => {
