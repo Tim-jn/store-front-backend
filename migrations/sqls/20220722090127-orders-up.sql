27efe527aec51328330651d68f96c2dde83aa14c
Tim <Tim280696@hotmail.com>
2022-08-04 13:27:03 +0200

    update migrations

--- migrations/sqls/20220722090127-orders-up.sql
+++ migrations/sqls/20220722090127-orders-up.sql
@@ -1,1 +1,1 @@
-CREATE TABLE orders(id SERIAL PRIMARY KEY, product_id INTEGER REFERENCES products(id), quantity INTEGER, user_id INTEGER REFERENCES users(id), status VARCHAR(10));
+CREATE TABLE orders (id SERIAL PRIMARY KEY, status VARCHAR(50), user_id integer REFERENCES users(id));
