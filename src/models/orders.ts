69fc95fd444b0d45c02ffa1229000d429fb9585a
Tim <Tim280696@hotmail.com>
2022-08-04 13:27:41 +0200

    orders: add func to create orders

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -3,19 +3,24 @@ import client from "../database";
 
 export type Order = {
   id?: number;
-  product_id: number;
-  quantity: number;
   user_id: number;
   status: string;
 };
 
+export type OrderProducts = {
+  id?: number;
+  product_id: number;
+  quantity: number;
+  order_id: number;
+};
+
 export class OrdersStore {
-  async show(id: number): Promise<Order[]> {
+  async show(user_id: number): Promise<Order[]> {
     try {
       // @ts-ignore
       const conn = await client.connect();
       const sql = `SELECT * FROM orders WHERE user_id=($1)`;
-      const result = await conn.query(sql, [id]);
+      const result = await conn.query(sql, [user_id]);
       conn.release();
 
       return result.rows;
@@ -24,18 +29,32 @@ export class OrdersStore {
     }
   }
 
-  async addOrder(order: Order): Promise<Order> {
+  async create(order: Order): Promise<Order> {
     try {
       const conn = await client.connect();
 
-      const sql = `INSERT INTO orders (quantity, user_id, product_id, status) VALUES ($1, $2, $3, $4)`;
+      const sql = `INSERT INTO orders (user_id, status) VALUES ($1, $2)`;
+      const result = await conn.query(sql, [order.user_id, order.status]);
+
+      conn.release();
+
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Could not create order ${err}`);
+    }
+  }
+
+  async addOrder(order: OrderProducts): Promise<Order> {
+    try {
+      const conn = await client.connect();
+
+      const sql = `INSERT INTO order_products (quantity, order_id, product_id) VALUES ($1, $2, $3)`;
       const result = await conn.query(sql, [
         order.quantity,
-        order.user_id,
+        order.order_id,
         order.product_id,
-        order.status,
       ]);
-      
+
       conn.release();
 
       return result.rows[0];
