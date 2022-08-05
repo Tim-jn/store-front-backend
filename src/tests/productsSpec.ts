4c0c43425a8b40929ace5366b16734248a50de8e
Tim <Tim280696@hotmail.com>
2022-08-05 15:36:32 +0200

    productsSpec: update tests

--- src/tests/productsSpec.ts
+++ src/tests/productsSpec.ts
@@ -12,22 +12,6 @@ describe("Product Model", () => {
     await store.create(newProduct);
   });
 
-  it("should have an index method", () => {
-    expect(store.index).toBeDefined();
-  });
-
-  it("should have a show method", () => {
-    expect(store.show).toBeDefined();
-  });
-
-  it("should have a create method", () => {
-    expect(store.create).toBeDefined();
-  });
-
-  it("should have a delete method", () => {
-    expect(store.delete).toBeDefined();
-  });
-
   it("create method should add a product", async () => {
     const product = await store.create(newProduct);
 
